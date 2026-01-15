fd6b16ec96d12a9d1fb7bcb68697217a56e4d286
ethnosdev <contact@ethnos.dev>
2026-01-15 13:29:12 +0800

    initial dart cli project

---
+++ isochron_cli/bin/isochron_cli.dart
@@ -0,0 +1,5 @@
+import 'package:isochron_cli/isochron_cli.dart' as isochron_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${isochron_cli.calculate()}!');
+}
---
+++ isochron_cli/test/isochron_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:isochron_cli/isochron_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
